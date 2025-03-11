2b9cb380f427a0f03bdb49fe5e882558f94ec9fe
DrewMelohn <drewmelohn5@gmail.com>
2025-03-10 20:08:27 -0500

    register.html added

--- app.py
+++ app.py
@@ -25,7 +25,27 @@ def check_login():
 
 @app.route("/login/"), methods=["GET"])
 def login():
-    return render_template ("login.html")
+    return render_template("login.html")
+
+@app.route("/login/", methods=["POST"])
+def do_login():
+    username = request.form["username"]
+    password = request.form["password"]
+    users = get_users()
+    login = False
+    for user in users:
+        if user.username == username and user.password == password:
+            session["username"] = username
+            login = True
+            break
+    if login:
+        return redirect(url_for("item_sum_page.html"))
+    else:
+        return render_template("login.html", message="Wrong username/password")
+
+@app.route("/register/", methods=["GET"])
+def register():
+    return render_template("register.html")
 
 
 @app.route("/items_sum_page/"), methods=["GET"])
